62dc5161b40b930d7cbc3dce21adc9d89d8c4422
François-Xavier Guillois <40766799+tnntwister@users.noreply.github.com>
2022-04-09 10:15:38 +0200

    settings bug

--- join/urls.py
+++ join/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from join import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', views.index, name='index'),
     path('join', views.join, name='join'),
     path('contribute', views.contribute, name='contribute'),
